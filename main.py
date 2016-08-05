fadffdb1b5a137105d2dde5225728cb87012dca4
Ricky-Hao <a471558277@gmail.com>
2016-08-05 06:04:21 +0000

    Add main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+from from aliyunsdkcore import client
+from aliyunsdkalidns.request.v20150109 import UpdateDomainRecordRequest
+from aliyunsdkalidns.request.v20150109 import DescribeDomainRecordsRequest
+import json
+
+if __name__ =='__main__':
+    config=json.load(file("config.json"))
